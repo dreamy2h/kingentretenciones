9f4b3076ed4e65dc19d2f51e3343c3d20628328a
Zamyr <zamyrpedreros@outlook.com>
2018-09-11 15:51:47 -0400

    avance

--- config/modificar_index.php
+++ config/modificar_index.php
@@ -11,7 +11,14 @@
 		<div class="row">
 			<div class="col-lg-2 col-md-2"></div>
 			<div class="col-lg-8 col-md-8 col-sm-12">
-				<span class="label label-default">Slider 1</span>
+				<div class="row">
+					<h1><span class="label col-lg-12 col-md-12 col-sm-12">Slider 1</span></h1>
+				</div>
+				<div class="row">
+					<span class="label col-lg-4 col-md-4 col-sm-12">Slider 1</span>
+					<input type="text" name="txt_num_sliders" id="txt_num_sliders" class="form-control col-lg-4 col-md-4 col-sm-12">
+					<button type="button" class="btn btn-default col-lg-4 col-md-4 col-sm-12">Agregar</button>
+				</div>
 			</div>
 			<div class="col-lg-2 col-md-2"></div>
 		</div>
