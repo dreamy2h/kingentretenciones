6e01c0965a9440490e5df798b6fcbbb648303585
Zamyr <zamyrpedreros@outlook.com>
2018-09-25 15:47:46 -0400

    prueba funcional de fileinput

--- config/modificar_index.php
+++ config/modificar_index.php
@@ -77,7 +77,7 @@
 		      	</div>
 		      	<div class="modal-body">
 		        	<p id="prf_subttl_carpeta"></p>
-		        	<input id="fi_imagenes" type="file" multiple="true" name="imagenes[]" class="file-loading">
+		        	<input id="archivos" name="imagenes[]" type="file" multiple=true class="file-loading">
 		      	</div>
 		      	<div class="modal-footer">
 		        	<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -85,33 +85,34 @@
 		    </div>
 		</div>
 	</div>
-	<script type="text/javascript" src="js/modificar_index.js"></script>
 </body>
 <?php 	
-	$directory = "imagenes/";      
+	$directory = "imagenes_/";      
 	$images = glob($directory . "*.*");
-?>
+	?>
 	
-<script>
-	$("#fi_imagenes").fileinput({
-		uploadUrl: "upload.php", 
-    	uploadAsync: false,
-    	minFileCount: 1,
-    	maxFileCount: 20,
-		showUpload: true, 
-		showRemove: false,
-		initialPreview: [
-			<?php foreach($images as $image){?>
-				"<img src='<?php echo $image; ?>' height='120px' class='file-preview-image'>",
-			<?php } ?>],
-    	initialPreviewConfig: [<?php foreach($images as $image){ $infoImagenes=explode("/",$image);?>
-			{caption: "<?php echo $infoImagenes[1];?>",  height: "120px", url: "borrar.php", key:"<?php echo $infoImagenes[1];?>"},
-		<?php } ?>]
+	<script>
+	$("#archivos").fileinput({
+	uploadUrl: "upload.php", 
+    uploadAsync: false,
+    minFileCount: 1,
+    maxFileCount: 20,
+	showUpload: false, 
+	showRemove: false,
+	initialPreview: [
+	<?php foreach($images as $image){?>
+		"<img src='<?php echo $image; ?>' height='120px' class='file-preview-image'>",
+	<?php } ?>],
+    initialPreviewConfig: [<?php foreach($images as $image){ $infoImagenes=explode("/",$image);?>
+	{caption: "<?php echo $infoImagenes[1];?>",  height: "120px", url: "borrar.php", key:"<?php echo $infoImagenes[1];?>"},
+	<?php } ?>]
 	}).on("filebatchselected", function(event, files) {
 	
-	$("#fi_imagenes").fileinput("upload");
+	$("#archivos").fileinput("upload");
 	
 	});
 	
 	</script>
+
+	<script type="text/javascript" src="js/modificar_index.js"></script>
 </html>
