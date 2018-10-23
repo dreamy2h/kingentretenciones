c99d43ac050915aaa5d902a5f798ead1332cc45c
Zamyr Pedreros <zamyrpedrero@outlook.com>
2018-10-22 22:40:33 -0300

    avance se comienza a editar el index

--- js/index.js
+++ js/index.js
@@ -30,43 +30,57 @@ function cargar_eventos() {
 			}
 
 			eventos += "</div>";
-
 			$("#eventos").append(eventos);
+
+			return true;
  		}
 	});
 }
+
+function cargar_promociones() {
+	$.ajax({
+ 		type: "GET",
+ 		dataType: "json",
+ 		url: "config/sql/consultas/consulta_promociones.php",
+ 		async: false,
+ 		success: function(data) {
+ 			var promociones = "<ol class='carousel-indicators'>";
+ 			for (var i = 0; i < data.length; i++) {
+ 				if (i === 0) {
+ 					promociones += "<li data-target='#miSlide2' data-slide-to='" + i + "' class='active'></li>";
+ 				} else {
+ 					promociones += "<li data-target='#miSlide2' data-slide-to='" + i + "'></li>";
+ 				}
+			}
+
+			promociones += "</ol>";
+			promociones += "<div class='carousel-inner'>";
+
+			for (var i = 0; i < data.length; i++) {
+				if (i === 0) {
+					promociones += "<div class='item active'>";
+	                promociones += "<img src='config/imagenes/" + data[i].id_carpeta + "/" + data[i].nombre_imagen + "' class='adaptar'>";
+                    promociones += "</div>";
+				} else {
+					promociones += "<div class='item'>";
+	                promociones += "<img src='config/imagenes/" + data[i].id_carpeta + "/" + data[i].nombre_imagen + "' class='adaptar'>";
+                    promociones += "</div>";
+				}
+			}
+
+			promociones += "</div>";
+			console.warn(promociones);
+			$("#promociones").append(promociones);
+			return true;
+ 		}
+	});
+}
+
 $(document).ready(function() {
 	cargar_eventos();
+	cargar_promociones();
 });
 
 jQuery(window).load(function () {
     $('#preloader-container').delay(650).fadeOut('slow');
-});
-
-jQuery(document).ready(function ($) {
-    $('.image-popup').magnificPopup({
-        type: 'image',
-        removalDelay: 300,
-        mainClass: 'mfp-with-zoom',
-        titleSrc: 'title',
-        gallery: {
-            enabled: true
-        },
-        zoom: {
-            enabled: true, // By default it's false, so don't forget to enable it
-
-            duration: 300, // duration of the effect, in milliseconds
-            easing: 'ease-in-out', // CSS transition easing function
-
-            // The "opener" function should return the element from which popup will be zoomed in
-            // and to which popup will be scaled down
-            // By defailt it looks for an image tag:
-            opener: function (openerElement) {
-                // openerElement is the element on which popup was initialized, in this case its <a> tag
-                // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-                // return openerElement.is('img') ? openerElement : openerElement.find('img');
-                return openerElement.is('img') ? openerElement : openerElement.find('img');
-            }
-        }
-    });
 });
